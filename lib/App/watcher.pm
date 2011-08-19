661f946dfcab81b52b934d5e976f2d48b41e0a0c
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2011-08-19 10:44:13 +0000

    Import of TOKUHIROM/App-watcher-0.01 from CPAN.
    gitpan-cpan-distribution: App-watcher
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TOKUHIROM/App-watcher-0.01.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

---
+++ lib/App/watcher.pm
@@ -0,0 +1,23 @@
+package App::watcher;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::watcher -
+
+=head1 SYNOPSIS
+
+  use App::watcher;
+
+=head1 DESCRIPTION
+
+App::watcher is
+
+=cut
